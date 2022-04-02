1b4fbb86a14c8484eca51d96b6d58c96f70d2556
MakarenkoYevhenii <92297867+MakarenkoYevhenii@users.noreply.github.com>
2022-04-02 11:16:53 +0300

    dfgfd
    gdfg

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ const Review = lazy(() => import("./rewiev/Review"));
 const FilmSearch = lazy(() => import("../pages/moviesSearch/MoviesSearchPage"));
 const HeaderMenu = lazy(() => import("../pages/headerPage/HeaderPage"));
 const SingleFilmPage = lazy(() => import("../pages/singleFilm/SingleFilmPage"));
-const MainPage = lazy(() => import("./MainPage/MainPage"));
+const MainPage = lazy(() => import("./MainPage/MainPage.jsx"));
 
 export const App = () => {
   return (
