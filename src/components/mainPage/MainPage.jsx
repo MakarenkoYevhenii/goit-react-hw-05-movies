4a4e25c973394d95b8275b981f5d97da36a29f16
MakarenkoYevhenii <92297867+MakarenkoYevhenii@users.noreply.github.com>
2022-04-02 10:45:19 +0300

    fdgdf
    gdf

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -1,4 +1,4 @@
-import { trendsMovie } from '../../shared/services/getMovies';
+import { trendsMovie } from '../../shared/services/getMovies.js';
 import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import style from '../MainPage/mainPage.module.css';
